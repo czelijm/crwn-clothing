e6f55e34b732796537eb6cd538359c83a58a0f47
czelijm <czelijm@outlook.com>
2021-04-03 14:37:03 +0200

    Added Header Component to app

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,18 +5,20 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     // render only one Route and go out the swtich
-    <Switch>
-      <div>
-        {/* renderIfPathIsExactTheSameAsPathParameter relativePathFromHere componentWeWAntTorender */}
+    <div>
+      <Header/>
+      <Switch>
+        {/* renderIfPathIsExactTheSameAsPathParameter relativePathFromHere componentWeWAntTorender */} 
         <Route exact={true}  path='/' component={HomePage} /> 
         <Route path='/shop' component={ShopPage} />
-      </div>
-    </Switch>
+      </Switch>
+    </div>
   );
 }
 
