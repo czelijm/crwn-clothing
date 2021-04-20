c58220fa737d7b840a84f1a1bfb90b841e634806
czelijm <czelijm@outlook.com>
2021-04-20 19:17:53 +0200

    Added cart-icon and cart dropdown components

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -6,6 +6,8 @@ import './header.style.scss';
 
 import { ReactComponent as Logo  } from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 const Header = ({currentUser}) => (
     <div className='header'>
@@ -30,7 +32,9 @@ const Header = ({currentUser}) => (
                     SIGN IN
                 </Link>
             }
+            <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 )
 
