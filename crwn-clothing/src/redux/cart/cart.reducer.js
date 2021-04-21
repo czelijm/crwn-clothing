ac4185a07a1fc606c954d953db512bd7fe1ee544
czelijm <czelijm@outlook.com>
2021-04-21 17:32:26 +0200

    Added AddToCart logic

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
 import {CartActionTypes} from "./cart.types";
 
+import {addItemToCart} from './cart.utils'
+
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,7 +13,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems,action.payload)
+            };
         default:
             return state;
     }
@@ -25,13 +33,14 @@ export default cartReducer;
         1. Define initial state - json object
         2. define Reducer function - switch case of action type
     3. Action
-        1. Define Action - as a function
+        1. Define Action - as a function that return json object
     4. in component in which you want to access action with
         A. mapDispatchToProps - inside define json object 
             { 
                 ACTION_NAME: (dispatch) => dispatch(ACTION_NAME())
             }
         B
-    5. connect(mapStateToProps,mapDispatchToProps,...otherParameters)(COMPONENT_NAME)
+    5. in component that we want to use it
+        1. add as export default connect(mapStateToProps,mapDispatchToProps,...otherParameters)(COMPONENT_NAME)
     6. add reducer to rootreducer
 */
