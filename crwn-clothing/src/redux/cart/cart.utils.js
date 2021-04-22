759c6bbcec91e04c0cd861cf0af530eef65686b6
czelijm <czelijm@outlook.com>
2021-04-23 00:27:29 +0200

    Added checkout-item component's removeItem and addItem functionality

--- crwn-clothing/src/redux/cart/cart.utils.js
+++ crwn-clothing/src/redux/cart/cart.utils.js
@@ -1,3 +1,5 @@
+import CartItem from "../../components/cart-item/cart-item.component";
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     
     const existingItem = cartItems.find(cartItem => cartItem.id===cartItemToAdd.id);
@@ -11,6 +13,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
             
             );
     }
-    return [...cartItems, {...cartItemToAdd, quantity:1 }];
+    return [...cartItems, {...cartItemToAdd, quantity:1 }];   
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingItem = cartItems.find( 
+        cartItem => cartItem.id === cartItemToRemove.id
+    );
+
+    if(existingItem.quantity===1){
+        return cartItems.filter(cartItem=>cartItem.id!==cartItemToRemove.id)
+    }
+    
+    return cartItems.map(cartItem=>
+        cartItem.id===cartItemToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity - 1}
+        :
+        cartItem 
+    )
     
 }
