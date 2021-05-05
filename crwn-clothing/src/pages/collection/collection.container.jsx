465215ebf2f97965fe64c0509aaba8e5bf578ad7
czelijm <czelijm@outlook.com>
2021-05-05 20:32:03 +0200

    initial commit

--- crwn-clothing/src/pages/collection/collection.container.jsx
+++ crwn-clothing/src/pages/collection/collection.container.jsx
@@ -2,6 +2,7 @@ import {connect} from 'react-redux'
 import {compose} from 'redux'
 import {createStructuredSelector} from 'reselect'
 
+
 import { selectIsCollectionLoaded } from '../../redux/shop/shop.selector';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionPage from './collection.component'
